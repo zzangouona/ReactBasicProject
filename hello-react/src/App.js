cafb277a674ebe5d7d0361e12f6ff0b6764666a8
zzanguna <zzanguna>
2020-12-21 20:05:06 +0900

    hello-react 프로젝트 - 감싸인 요소 에러

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -9,23 +9,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>리액트 안녕!</h1>
+      <h2>잘 작동하니?</h2>1
+      );
 }
 
 export default App;
