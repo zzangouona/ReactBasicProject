a7b84bf13f66670c2f5db5810bb9144ca531fea6
zzanguna <zzanguna>
2020-12-21 21:01:37 +0900

    hello-react 프로젝트 - undefined 를 렌더링 하지 않기!

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -7,21 +7,13 @@ import './App.css';
 // 이렇게 import 하여 파일을 불러오는 것을 웹팩에서 웹팩의 로더라는 기능이 담당하고 있다.
 
 function App() {
-    // ES6 변수
-    const name = "정윤아";
-    // var 는 scope 단위가 함수 단위이다. 그리고 const, let은 스코프가 블록단위이다.
-    // var 보다 const 와 let을 사용하자!
-    // 기본적으로 const를 사용하고 해당값을 바꿔야할 경우 let을 사용하자.
-    return (
-        <>
-            {/* And 연산자(&&)를 사용한 조건부 렌더링 */}
-            {name === "정윤아" && <h1>{name} 안녕!</h1>}
-            {/*
-                true 일 경우 해당 렌더링을 할 수 있지만 false 일 경우 null이기에 화면에 아무것도 나타나지 않는다.
-            */}
-            <h2>잘 작동하니?</h2>
-        </>
-    );
+    // undefined 를 렌더링 하지 않기!
+    const name = undefined;
+    return name;
+    //return name || "값이 undefined 입니다."
+    // jsx 로 undefined 렌더링
+    //return <>{name}</>
+    //return <>{name || "리액트"}</>
 }
 
 export default App;
