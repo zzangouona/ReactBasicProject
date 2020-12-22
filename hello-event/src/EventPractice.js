fb9e50cb3d0bae8a399557d1730a8e58ca2752ac
zzanguna <yajung0203@naver.com>
2020-12-23 00:40:12 +0900

    hello-event 1. react 이벤트 - 화살표 함수를 이용해서 이벤트 함수 처리하기

--- hello-event/src/EventPractice.js
+++ hello-event/src/EventPractice.js
@@ -11,27 +11,28 @@ class EventPractice extends Component {
   // 이벤트에 함수 형태로 처리
 
   // 생성자에서 메서드 바인딩하기
-  constructor(props) {
-    super(props);
-    // 함수가 호출될때 this는 호출부에 따라 결정되므로, 클래스의 임의 메서드가 특정 html 요소의 이벤트로 등록되는 과정에서 메서드와 this의 관계를 끊어져 버린다.
-    // 따라서 임의 메서드가 이벤트로 등록되어도 this를 컴포넌트 자신으로 제대로 가리키기 위해서는 메서드를 this와 바인딩하는 ㅏㅈㄱ업이 필요
-    // 바인딩하지 않으면 this가 undefined를 가르킨다.
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   // 함수가 호출될때 this는 호출부에 따라 결정되므로, 클래스의 임의 메서드가 특정 html 요소의 이벤트로 등록되는 과정에서 메서드와 this의 관계를 끊어져 버린다.
+  //   // 따라서 임의 메서드가 이벤트로 등록되어도 this를 컴포넌트 자신으로 제대로 가리키기 위해서는 메서드를 this와 바인딩하는 ㅏㅈㄱ업이 필요
+  //   // 바인딩하지 않으면 this가 undefined를 가르킨다.
+  //   this.handleChange = this.handleChange.bind(this);
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
 
-  handleChange(e) {
+  // 화살표 함수를 사용하면 생성자에서 메서드 바인딩 하지 않아도 된다.
+  handleChange = (e) => {
     this.setState({
       message: e.target.value,
     });
-  }
+  };
 
-  handleClick(e) {
+  handleClick = (e) => {
     alert('현재 글자는 => ' + this.state.message);
     this.setState({
       message: '',
     });
-  }
+  };
 
   render() {
     const { message } = this.state;
