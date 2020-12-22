28e75de6ace49a31a48d59ba39a62fc25b4912fe
zzanguna <yajung0203@naver.com>
2020-12-23 00:29:05 +0900

    설정 테스트

---
+++ react-eventpj/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["react-app", "eslint:recommended", "plugin:react/recommended"],
+  rules: {
+    "react/prop-types": 0,
+    "react/prefer-stateless-function": 0,
+    "react/jsx-one-expression-per-line": 0,
+    "no-console": "off",
+    "no-unused-vars": "off",
+    "arrow-body-style": "off",
+    "react/jsx-filename-extension": [
+      "warn",
+      { extensions: [".js", ".jsx", ".svg"] },
+    ],
+  },
+};
