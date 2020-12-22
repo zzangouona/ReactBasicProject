e5efe6ba6877d0aa6aa4246521da94d687132850
zzanguna <yajung0203@naver.com>
2020-12-23 02:13:11 +0900

    hello-event 2. Ref - ref을 사용하는 기준 - ref 사용해서 input focus 처리 - 컴포넌트에 ref 달기

--- hello-event/src/App.js
+++ hello-event/src/App.js
@@ -4,10 +4,20 @@ import Say from './Say';
 import EventPractice from './EventPractice';
 import EventPracticehooks from './EventPracticehooks';
 import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
 class App extends Component {
+  // 컴포넌트에 ref 달기
+  // 해당 컴포넌트 내부의 메서드 및 멤버 변수에 접근할 수 있다.
   render() {
-    return <ValidationSample />;
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollRef = ref)} />
+        <button onClick={() => this.scrollRef.scrollToBottom()}>
+          맨 밑으로 이동
+        </button>
+      </div>
+    );
   }
 }
 
--- hello-event/src/ValidationSample.js
+++ hello-event/src/ValidationSample.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './ValidationSample.css';
 
-// ref 사용해서 이벤트 처리
+// ref 사용해보기
 class ValidationSample extends Component {
   state = {
     password: '',
     clicked: false,
     validated: false,
   };
+
   // ref을 사용하는 기준은 DOM을 직접적으로 건들어야하는 경우,
   // react는 ref 사용하지 않고도 state을 변경가능하다.
 
@@ -23,6 +24,7 @@ class ValidationSample extends Component {
       clicked: true,
       validated: this.state.password === '0000',
     });
+    this.input.focus();
   };
 
   render() {
@@ -30,7 +32,7 @@ class ValidationSample extends Component {
     return (
       <div>
         <input
-          ref={this.input}
+          ref={(ref) => (this.input = ref)}
           name={'password'}
           type={'password'}
           value={password}
