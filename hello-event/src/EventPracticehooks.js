759bc13131718b8da256bfe089eb199264c014f2
zzanguna <yajung0203@naver.com>
2020-12-23 01:23:04 +0900

    hello-event 1. react 이벤트 - 함수형 컴포넌트에서 input 여러개 일경우 처리

--- hello-event/src/EventPracticehooks.js
+++ hello-event/src/EventPracticehooks.js
@@ -2,15 +2,30 @@ import React, { useState } from 'react';
 
 //함수형 컴포넌트로 이벤트 처리하기
 const EventPracticehooks = () => {
-  const [message, setMessage] = useState('');
-  const [username, setUserName] = useState('');
-  const onChangeMessage = (e) => setMessage(e.target.value);
-  const onChangeUserName = (e) => setUserName(e.target.value);
+  // const [message, setMessage] = useState('');
+  // const [username, setUserName] = useState('');
+  // const onChangeMessage = (e) => setMessage(e.target.value);
+  // const onChangeUserName = (e) => setUserName(e.target.value);
 
+  // input 다수 일 경우 처리
+  const [form, setForm] = useState({
+    username: '',
+    message: '',
+  });
+  const { username, message } = form;
+  const onChange = (e) => {
+    const nextForm = {
+      ...form, // 기존 form 내용을 복사
+      [e.target.name]: e.target.value, // 원하는 값 덮어씌우기
+    };
+    setForm(nextForm);
+  };
   const onClick = (e) => {
     alert(`현재 데이터는 username = ${username}, message = ${message}`);
-    setUserName('');
-    setMessage('');
+    setForm({
+      username: '',
+      message: '',
+    });
   };
 
   const onKeyPress = (e) => {
@@ -24,14 +39,14 @@ const EventPracticehooks = () => {
         name="username"
         placeholder="사용자명을 입력하세요."
         value={username}
-        onChange={onChangeUserName}
+        onChange={onChange}
       />
       <br />
       <input
         name="message"
         placeholder="메세지를 입력하세요."
         value={message}
-        onChange={onChangeMessage}
+        onChange={onChange}
         onKeyPress={onKeyPress}
       />
       <br />
