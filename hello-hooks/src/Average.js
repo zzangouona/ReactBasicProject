ff820cd03d37d4d2f7eb554424d5bed7376bff6e
zzanguna <yajung0203@naver.com>
2020-12-23 19:07:21 +0900

    hello-hooks - hooks useCallback 으로 함수 표현식 최적화하기

--- hello-hooks/src/Average.js
+++ hello-hooks/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
 // useMemo
 const getAverage = (numbers) => {
@@ -12,15 +12,21 @@ const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
 
-  const onChange = (e) => {
-    setNumber(e.target.value);
-  };
+  // useCallback으로 최적화하기
+  // 함수 표현식으로 선언한 함수는 컴포넌트가 리렌더링될때마다 이 함수들이 새로 생성된다. -> 최적화 방법 useCallback
+  // useCallback의 첫번째 파라미터는 생성하고 싶은 함수를 넣고, 두번째 파라미터는 배열을 넣는다
+  // 이 배열에는 어떤값이 바뀌었을때 함수를 새로 생성해야하는 명시해향한다.
+  // 함수 내부에서 상태 값에 의존해야할 때는 그 값을 반드시 두번재 파라미터 안에 포함시켜 주어야한다.
 
-  const onInsert = () => {
+  const onChange = useCallback((e) => {
+    setNumber(e.target.value);
+  }, []); // 컴포넌트가 처음 렌더링될 때만 함수생성
+
+  const onInsert = useCallback(() => {
     const nextList = list.concat({ id: list.length, value: Number(number) });
     setList(nextList);
     setNumber("");
-  };
+  }, [number, list]); //number, list가 바뀌었을대만 함수 생성
 
   const onKeyPress = (e) => {
     if (e.key === "Enter") onInsert();
