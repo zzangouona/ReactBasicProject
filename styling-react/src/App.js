41b6e3cdbd503940f131f99b6de2f33edb8259cd
zzanguna <yajung0203@naver.com>
2021-01-07 03:03:29 +0900

    styling-react - css module 사용해서 style 처리하기 - 글로벌 css 작성

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
 
 function App() {
   return (
     <div>
-      <SassComponent />
+      <CSSModule />
     </div>
   );
 }
