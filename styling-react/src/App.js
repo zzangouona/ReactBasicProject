1c3d81db036f161db051f4f9c9131b904dfb722b
zzanguna <yajung0203@naver.com>
2021-01-07 16:35:00 +0900

    styling-react - 자바스크립트 파일 안에서 스타일 선언

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -2,11 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import SassComponent from "./SassComponent";
 import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponent";
 
 function App() {
   return (
     <div>
-      <CSSModule />
+      <StyledComponent />
     </div>
   );
 }
