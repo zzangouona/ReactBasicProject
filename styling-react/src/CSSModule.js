d5d254ad323129b17c07fd5517a22e25f829a320
zzanguna <yajung0203@naver.com>
2021-01-07 14:10:49 +0900

    styling-react - class가 여러개일 경우 처리 - classNames bind 처리

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -1,8 +1,14 @@
 import React from "react";
+import classNames from "classnames/bind";
 import styles from "./CSSMoule.module.css";
+
+const cx = classNames.bind(styles); // 미리 클래스를 받아오도록 설정한다.
 const CSSModule = () => {
   return (
-    <div className={styles.wrapper}>
+    // 클래스가 여러개 생길 경우
+    //<div className={`${styles.wrapper} ${styles.inverted}`}>
+    // calssnames 의 bind 함수로 처리한 경우
+    <div className={cx("wrapper", "inverted")}>
       안녕하세요 저는 <span className={"something"}>CSS Module!</span>
     </div>
   );
